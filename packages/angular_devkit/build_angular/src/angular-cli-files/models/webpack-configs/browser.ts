94d7a45055a7df39f11188cc5648be1ec5dabd02
Robin Hassan <ra.onerobin@gmail.com>
2018-09-26 02:45:24 +0800

    fix web3 dependency issue

--- packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/browser.ts
+++ packages/angular_devkit/build_angular/src/angular-cli-files/models/webpack-configs/browser.ts
@@ -142,6 +142,16 @@ export function getBrowserConfig(wco: WebpackConfigOptions) {
         sri: buildOptions.subresourceIntegrity,
       }),
     ]),
-    node: false,
+    node:  {
+      fs: 'empty',
+      global: true,
+      crypto: 'empty',
+      tls: 'empty',
+      net: 'empty',
+      process: true,
+      module: false,
+      clearImmediate: false,
+      setImmediate: false
+    }
   };
 }
